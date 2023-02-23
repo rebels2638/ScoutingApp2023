3d8668333d6a7aa7e584946a20c30af4f872fcea
noob82 <72877382+noob822@users.noreply.github.com>
2023-02-23 01:58:44 +0000

    endgame buttons

--- Routes/ScoutComponents/EndGame.js
+++ Routes/ScoutComponents/EndGame.js
@@ -11,6 +11,8 @@ import ScoutingColors from "../../Config/ScoutingColors";
 import { useDispatch } from "react-redux";
 import { setDefault } from "../../Redux/Features/dataSlice.js";
 import BoolButton from "../../Components/Buttons/BoolButton.js";
+import NumButton from "../../Components/Buttons/NumButton.js";
+import RadioButton from "../../Components/Buttons/RadioButton.js";
 import { useTheme } from "@react-navigation/native";
 
 export default function Endgame() {
@@ -22,44 +24,23 @@ export default function Endgame() {
 	dispatch(setDefault([endgameID, 0]));
 
 	return (
-		<View style={[styles.container, {backgroundColor: colors.background}]}>
-			<Text style={{ textAlign: "center", fontSize: 35, fontWeight: "bold" }}>Endgame</Text>
-
-			<View style={[styles.endgameContainer, {borderColor: colors.border, backgroundColor: colors.card}]}>
-				<View style={{flexDirection: "row", justifyContent: "space-evenly"}}>
-					<View style={{justifyContent: "space-evenly", alignItems: "center"}}>
-						<BoolButton id="FailedClimbPosition" width={120} bgc="red">Failed</BoolButton>
-						<Timer id="Time"/>
-					</View>
-
-					<ClimbPosition id="ClimbPosition" bgc="orange" />
-				</View>
-				
-				
-				
-
-
-				<View style={styles.climbComments}>
-					<Text style={{ fontWeight: "bold", fontSize: 20 }}>Comments</Text>
-					<Text style={{ fontSize: 12, textAlign: "center" }}>
-							Add any comments that you feel are useful. Do they attempt to climb but fall?
-							Do they get in the way of other robots? Do they swing a lot on the climb?
-							Anything else that shows evidence of good/poor performance?
-					</Text>
-					<CustomTextBox
-						multi={true}
-						id="EndgameComments"
-						width={900}
-						height={250}
-						placeholder="Type your comments here..."
-						borderRadius={10}
-					/>
-				</View>
-			</View>
+		<View>
+			<NumButton id="NumRoboClimbed" width={160}>Number of Robots climbed</NumButton>
+			<NumButton id="TimeToEngaged" width={160}>Endgame Time to Engaged</NumButton>
+			<RadioButton id="EndgameClimb" data={["None", "Docked", "Engaged", "Failed"]} bgc="orange" segmentedButton options={{flexDirection: "row"}}/>
+			<CustomTextBox
+				multi={true}
+				id="EndgameComments"
+				width={900}
+				height={250}
+				placeholder="Type your comments here..."
+				borderRadius={10}
+			/>
 		</View>
 	);
 	
 }
+
 
 const styles = StyleSheet.create({
 	climbComments: {
