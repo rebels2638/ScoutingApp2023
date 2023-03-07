412c6c58013bc21e6e27f04b41143fa7395008fd
Mustan Pathetic <patheticmustan@gmail.com>
2023-03-07 04:48:31 +0000

    grid, but the code is written horribly

--- Components/OneUse/Grid.js
+++ Components/OneUse/Grid.js
@@ -27,8 +27,8 @@ function ConeButton(props) {
 			onPress={() => { dispatch(setKeyPair([props.id, (value+1)%2])); }}
 		>
 			{
-				value===0? (<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cone.png")} />) :
-				<Text style={{textAlign: "center", color: (value? "#000" : colors.text)}}>Empty</Text>
+				value===0? (<Text style={{textAlign: "center"}}>Empty</Text>) :
+				(<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cone.png")} />)
 			}
 		</Pressable>
 	);
@@ -49,9 +49,8 @@ function BoxButton(props) {
 			onPress={() => { dispatch(setKeyPair([props.id, (value+1)%2])); }}
 		>
 			{
-				value===0? (<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cube.png")} />) :
-				<Text style={{textAlign: "center", color: (value? "#000" : colors.text)}}>Empty</Text>
-				
+				value===0? (<Text style={{textAlign: "center"}}>Empty</Text>) :
+				(<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cube.png")} />)
 			}
 		</Pressable>
 	);
@@ -72,9 +71,9 @@ function ConeBoxButton(props) {
 			onPress={() => { dispatch(setKeyPair([props.id, (value+1)%3])); }}
 		>
 			{
-				value===0? (<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cone.png")} />) :
-				value===1? (<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cube.png")} />) :
-				<Text style={{textAlign: "center", color: (value? "#000" : colors.text)}}>Empty</Text>
+				value===0? (<Text style={{textAlign: "center"}}>Empty</Text>) :
+				value===1? (<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cone.png")} />) :
+				(<Image style={{flex: 1}} source={require("../../Assets/2023/2023Cube.png")} />)
 			}
 		</Pressable>
 	);
@@ -88,83 +87,44 @@ export default function Grid(props) {
 
 	// set default value
 	dispatch(setDefault([arenaID, 0]));
-	// since this isn't an input, no need to set default.
-	// get value from store
 	const selectedTeam = useSelector(selectID(arenaID));
-
-    // each item should be in the form
-    // { pos: (x, y), com: component }
-    // x and y being floats from 0-1. 0 will be top left, 1 will be bottom right.
-    // com should be the component to render
-    const items = props.items || [];
-    // hopefully this makes positioning easier
 
 	const width = 1415/1.5,
 		  height = 324/1.5;
 
-	const printDebugCoords = (e) => {
-		// When you click on the arena, print coordinates to console for positioning
-		let x = e.nativeEvent.offsetX / width,
-			y = e.nativeEvent.offsetY / height;
-
-		// truncate to two decimal places
-		// this is why i love javascript
-		[x, y] = [x, y].map(v => ~~(v*100)/100);
-		
-		// as a final note, components are positioned by their TOP LEFT CORNER, not the center.
-		console.log(x, y);
-	}
-
+	// 0 is Cone, 1 is Box, 2 is ConeBox
+	// i am so sorry for this code
+	const boxes = [[0, 1, 0,    0, 1, 0,    0, 1, 0],
+				   [0, 1, 0,    0, 1, 0,    0, 1, 0],
+				   [2, 2, 2,    2, 2, 2,    2, 2, 2]];
+	
 	return (
 		<View>
-			<ConeButton id="AutoGrid-0-0"/>
-			<BoxButton id="AutoGrid-0-1"/>
-			<ConeBoxButton id="AutoGrid-0-2"/>
-			<Pressable onPress={e => printDebugCoords(e)} >
-				<ImageBackground
-					source={selectedTeam? require("../../Assets/2023/BlueGrid.png") : require("../../Assets/2023/RedGrid.png")}
-					style={{ width: width, height: height, marginTop: 20 }}
-					imageStyle={{ borderRadius: 10, borderColor: colors.border }}
-				>
-					{/** this.props.items
-					 * in an effort to make item positioning easier, we have a custom arena wrapper
-					 * GridArena allows us to specify an X and a Y as a percentage (float 0-1)
-					 * It places the top left corner of the component at the specified coord
-					 * 
-					 * maybe we can change it to place the middle of the component at the coord?
-					 * we would need to know the height and width of the component though, so we'd 
-					 * need to render it once to get the dimensions, then again to re-position
-					 * 
-					 * follows the format
-					 * [{pos: (percentage, percentage), com: ()=>JSX}] 
-					 * 
-					 * GOTCHA: com needs to be a function. If you get an error about "expected string literal
-					 * 		but got JSX", just wrap the component in an arrow function or something
-					 *                             the fix
-					 * <Text>natsumi</Text>         ---->        () => <Text>natsumi</Text>
-					 **/
-					items.map((item, index) => {
-						// { pos: (x, y), com: component }
-						const [x, y] = item.pos;
-						
-						// if we're flipping the arena, position by bottom right corner instead of top left
-						const positionStyle = 
-							((selectedTeam == 0)? 
-								{ position: "absolute", left: width*x, top: height*y } :
-								{ position: "absolute", right: width*x, top: height*y });
-						
-						// each item in an iterable needs to have a unique key
-						// see https://stackoverflow.com/a/34868672
-						const key = `${item.pos}-ground beef-${index}`;
-
+			<ImageBackground
+				source={selectedTeam? require("../../Assets/2023/BlueGrid.png") : require("../../Assets/2023/RedGrid.png")}
+				style={{ width: width, height: height, marginTop: 20 }}
+				imageStyle={{ borderRadius: 10, borderColor: colors.border }}
+			>
+				{boxes.map((row, i) => 
+					row.map((c, o) => {
+						const positionStyle = {
+							position: "absolute",
+							left: (width/9)*o + (50/2),
+							top: (height/3)*i,
+							backgroundColor: "#DDDDDD88"
+						}
+					
+						// 0 is Cone, 1 is Box, 2 is ConeBox
 						return (
-							<View style={[positionStyle]} key={key}>
-								<item.com/>
+							<View style={positionStyle}>
+								{(c===0)? <ConeButton id={`AutoGrid-${i}-${o}`} /> :
+								(c===1)? <BoxButton id={`AutoGrid-${i}-${o}`} /> :
+										<ConeBoxButton id={`AutoGrid-${i}-${o}`} />}
 							</View>
 						);
-					})}
-				</ImageBackground>
-			</Pressable>
+					})
+				)}
+			</ImageBackground>
 		</View>
 	);
 }
@@ -176,7 +136,7 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		borderWidth: StyleSheet.hairlineWidth,
 		margin: 10,
-		height: 70,
-		width: 70
+		height: 50,
+		width: 50
 	}
 })
